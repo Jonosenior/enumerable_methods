cd27a1cf14f10f1a467052e1f40fdcc7e8a1f195
jonathan senior <jonosenior@gmail.com>
2017-10-25 13:46:07 +0200

    Added project and draft README

---
+++ README.md
@@ -0,0 +1,23 @@
+# Enumerable methods project
+
+These are my own versions of various Ruby iterator methods in the [Enumerator module](https://ruby-doc.org/core-2.4.2/Enumerable.html).
+
+Specifically, this project includes my versions of #each, #each_with_index, #select, #all?, #any?, #none?, #count, #map (#collect) and #inject (#reduce).
+
+## Installation
+
+<!-- Open your terminal/command line. Navigate to the directory where you want this project to live. Type:
+```
+$ git clone https://github.com/JonoSenior/enumerator_methods.git
+$ cd bubble_sort
+$ ruby bubble_sort.rb
+``` -->
+## Pre-project thoughts and motivations
+
+The aim of this project is to better understand how to use blocks and procs, and also to deepen my understanding of what it is that iterator methods are doing behind the scenes - to show they're not magic black boxes.
+
+## Post-project thoughts
+
+1. I developed a better understanding of procs and blocks, and why they're used.
+2. One of my biggest difficulties was making the methods flexible enough that they could work on Arrays, Hashes and Ranges. Working through this difficulty was very helpful to my understanding.
+3. I concentrated on refactoring my code to make it more concise and readable, for example by using one-line if statements, relying on truthy values to return true rather than spelling it out explicitly, etc.
---
+++ enumerable_methods.rb
@@ -0,0 +1,132 @@
+module Enumerable
+  def my_each
+    for element in self
+      yield element
+    end
+    self
+  end
+end
+
+module Enumerable
+  def my_each_with_index
+    i = 0
+    self.my_each do |element|
+      yield element, i
+      i += 1
+    end
+    self
+  end
+end
+
+
+module Enumerable
+  def my_select
+    answer = {} if self.class == Hash
+    answer = [] if self.class == Array
+    self.my_each do |element, value|
+      #answer.push(element) if yield(element) != false
+      answer << (element) if (yield(element) != false) && (self.class == Array)
+      if (yield(element, value) != false) && (self.class == Hash)
+        answer[element] = value
+      end
+    end
+    answer
+  end
+end
+
+
+module Enumerable
+  def my_all?
+    self.my_each do |element|
+      return false if !yield(element)
+    end
+    true
+  end
+end
+
+
+module Enumerable
+  def my_any?
+    self.my_each do |element|
+      return true if yield(element)
+    end
+    false
+  end
+end
+
+
+module Enumerable
+  def my_none?
+    self.my_each do |element|
+      return false if yield(element)
+    end
+    true
+  end
+end
+
+module Enumerable
+  def my_count(*arg1)
+    count = 0
+    self.my_each do |element|
+      if !block_given?
+        # puts "no block"
+        # puts "arg1: #{arg1.join("")} element: #{element}"
+        count +=1 if element == arg1.join("").to_i
+      else
+        count += 1 if yield(element)
+        # puts "block present"
+      end
+    end
+    count
+  end
+end
+
+module Enumerable
+  def my_map
+    new_object = []
+    self.my_each do |element|
+      new_object.push(yield element)
+    end
+    new_object
+  end
+end
+
+# Same as above, but can take either a proc or a block
+
+module Enumerable
+  def my_map(someProc = nil)
+    new_object = []
+    if someProc.class == Proc
+      self.my_each do |element|
+        new_object.push(someProc.call(element))
+      end
+    else
+      self.my_each do |element|
+        new_object.push(yield (element))
+      end
+    end
+    new_object
+  end
+end
+
+
+
+module Enumerable
+  def my_inject(running_total = 0)
+    self.my_each do |element|
+      running_total = yield(running_total, element)
+    end
+    running_total
+  end
+end
+
+# def multiply_els(array)
+#   array.my_inject(1) {|running_total, element| running_total * element}
+# end
+
+
+
+
+
+array = [1, 2, 3, 4, 5]
+my_array.my_each_with_index { |element, index| puts "#{element} and #{index}" }
